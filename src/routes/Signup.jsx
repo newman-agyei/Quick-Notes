2e41cfa0e6127b11df914bc6c0e48548d3ff3bb9
newman-agyei <agyeinewman@gmail.com>
2025-06-06 00:13:43 +0000

    added login and signup pages

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,6 +1,93 @@
-function Signup(){
-    return(
-        <h1>Signup</h1>
-    )
+import React, { useState } from "react";
+import { Link, Notebook } from "lucide-react";
+function Signup() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirm, setConfirmPassword] = useState();
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <div className="bg-white rounded-lg shadow-md p-8">
+        <div>
+          <Notebook
+            className="h-12 w-12
+         text-indigo-600 mb-2"
+          />
+          <h2 className="text-2xl font-bold text-gray-900">
+            Creeate your account
+          </h2>
+          <p className="text-gray-600">Start taking your note today</p>
+
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="you@example.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="***************"
+                required
+              />
+              <div className="mt-4">
+              <label
+                htmlFor="password-confirm"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Confirm Password
+              </label>
+              <input
+                id="password confirmed"
+                type="password"
+                value={passwordConfirm}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="***************"
+                required
+              />
+              </div>
+            </div>
+            <button
+              className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed mt-4"
+              type="submit"
+            >
+              Create Account
+            </button>
+          </form>
+
+          <div className="mt-6 text-center text-sm">
+            <p className="text-gray-600">
+              Don't have an account yet?
+              <Link
+                to="/login"
+                className="text-indigo-600 hover:text-indigo-800 font-medium"
+              >
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Signup;
